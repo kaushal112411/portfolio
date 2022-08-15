9a04e47985a9ac2102bce49b686fdfe7202aafb0
Kaushal112411 <Kaushal112411@gmai.com>
2022-08-16 03:48:04 +0530

    ua portfolio done

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -53,11 +53,12 @@ const SocialLinks = () => {
         <div className=' hidden lg:flex flex-col top-[35%] left-0 fixed '>
             <ul>
             {links.map((item)=>{
-                return <li key={item.id} className={'flex  justify-between items-center w-40 h-14 bg-gray-500 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300'+' '+item.style}>
+                return (
+                <li key={item.id} className={`flex  justify-between items-center w-40 h-14 bg-gray-500 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300  ${item.style}`}>
                  <a download={item.download} href={item.href} target='_blank' rel="noreferrer" className=' flex  justify-between  items-center  w-full  text-white'>
                    {item.child}
                  </a>
-             </li>
+             </li>)
             })}
                
             </ul>
